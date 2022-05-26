d6bc89fda795167f030209ec9ed6504d2c463df0
PRSoftRafael <bakaschevr@gmail.com>
2022-05-26 21:23:36 +0200

    final version

--- 2DJumpAndRunGame/Assets/Scripts/LevelSelect.cs
+++ 2DJumpAndRunGame/Assets/Scripts/LevelSelect.cs
@@ -10,28 +10,28 @@ public class LevelSelect : MonoBehaviour
         switch (this.gameObject.name)
         {
             case "Button 1":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level1");
                 break;
             case "Button 2":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level2");
                 break;
             case "Button 3":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level3");
                 break;
             case "Button 4":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level4");
                 break;
             case "Button 5":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level5");
                 break;
             case "Button 6":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level6");
                 break;
             case "Button 7":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level7");
                 break;
             case "Button 8":
-                SceneManager.LoadScene("Hazal level 3");
+                SceneManager.LoadScene("Level8");
                 break;
         }
     }
